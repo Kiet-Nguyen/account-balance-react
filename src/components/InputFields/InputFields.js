028b8a7fa8bf7adc29a8dd2435192a2043e13792
Kiet <bunbothomngon@gmail.com>
2018-10-31 14:45:45 +0200

    createRef

--- src/components/InputFields/InputFields.js
+++ src/components/InputFields/InputFields.js
@@ -1,33 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './InputFields.module.css';
 import PlusIcon from '../UI/PlusIcon/PlusIcon';
 
-const InputFields = () => (
-  <section className="bg-light-gray">
-    <div className="container">
-      <div className={ classes.add_items }>
-        <div className={ classes.input_item }>
-          <select className={ classes.item_type }>
-            <option value="income">Income</option>
-            <option value="expense">Expense</option>
-          </select>
-        </div>
+class InputFields extends Component {
+  selectOption = React.createRef();
+  descriptionInput = React.createRef();
+  amountInput = React.createRef();
+  plusIconRef = React.createRef();
 
-        <div className={ classes.input_item }>
-          <input type="text" className={ classes.description__input } placeholder="Description" />
-        </div>
+  onChangeTypeHandler = () => {
+    const selectOptionClassList = this.selectOption.current.classList;
+    const desInputClassList = this.descriptionInput.current.classList;
+    const amountInputClassList = this.amountInput.current.classList;
+    const plusIconRefClassList = this.plusIconRef.current.classList;
+    selectOptionClassList.toggle(`${classes.type_expense_focus}`);
+    desInputClassList.toggle(`${classes.type_expense_focus}`);
+    amountInputClassList.toggle(`${classes.type_expense_focus}`);
+    plusIconRefClassList.toggle(`${classes.add_circle_expense_color}`);
+  }
 
-        <div className={ classes.input_item }>
-          <input type="number" className={ classes.amount__input } placeholder="Value" />
-        </div>
+  render() {
+    return (
+      <section className="bg-light-gray">
+        <div className="container">
+          <div className={classes.add_items}>
+            <div className={classes.input_item}>
+              <select onChange={this.onChangeTypeHandler} ref={this.selectOption} className={classes.item_type}>
+                <option value="income">Income</option>
+                <option value="expense">Expense</option>
+              </select>
+            </div>
 
-        <button className={ [classes.btn, classes.input_item].join(' ')}>
-          <PlusIcon cssClass={ classes.add_circle } />
-        </button>
-      </div>
-    </div>
-  </section>
-);
+            <div className={classes.input_item}>
+              <input type="text" ref={this.descriptionInput} className={classes.description__input} placeholder="Description" />
+            </div>
+
+            <div className={classes.input_item}>
+              <input type="number" ref={this.amountInput} className={classes.amount__input} placeholder="Value" />
+            </div>
+
+            <button className={[classes.btn, classes.input_item].join(' ')}>
+              <PlusIcon plusIconRef={this.plusIconRef} cssClass={classes.add_circle} />
+            </button>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
 
 export default InputFields;
--- src/components/UI/PlusIcon/PlusIcon.js
+++ src/components/UI/PlusIcon/PlusIcon.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const PlusIcon = ({ cssClass }) => (
-  <svg className={ cssClass } width="100%" height="100%" viewBox="0 0 50 50">
+const PlusIcon = ({ cssClass, plusIconRef }) => (
+  <svg ref={plusIconRef} className={ cssClass } width="100%" height="100%" viewBox="0 0 50 50">
     <circle cx="25" cy="25" r="25" />
     <line style={{
       fill: 'none',
