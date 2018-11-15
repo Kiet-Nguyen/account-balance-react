a3b261848907bc9e5850158c762f4ea4e7db3b05
Kiet <bunbothomngon@gmail.com>
2018-11-15 18:35:13 +0200

    Updates

---
+++ precache-manifest.cad981344116c30a6946447459d2bccd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa786254fd5f3150cacc",
+    "url": "/account-balance-react/static/js/runtime~main.fa786254.js"
+  },
+  {
+    "revision": "9c9c0df413a56e0fd197",
+    "url": "/account-balance-react/static/js/main.9c9c0df4.chunk.js"
+  },
+  {
+    "revision": "778a6892300c5917d66f",
+    "url": "/account-balance-react/static/js/1.778a6892.chunk.js"
+  },
+  {
+    "revision": "9c9c0df413a56e0fd197",
+    "url": "/account-balance-react/static/css/main.bfdd2aa2.chunk.css"
+  },
+  {
+    "revision": "995a924d44e87cce2c775b2f3dcb9ed8",
+    "url": "/account-balance-react/index.html"
+  }
+];
